7ac087b5821d3ee21481678a36c4681ba5b9d3e4
priyaprafful <priyankamaheshwari.2021@gmail.com>
2018-11-20 16:43:46 +0200

    merge

--- bin/seed.js
+++ bin/seed.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
 const Food = require("../models/food-model");
-=======
-const City = require("../models/city-model");
->>>>>>> 82814d91af7f833e9b2046ea0c75ca889ebc3d01
 
 mongoose
   .connect(
@@ -186,161 +182,7 @@ const cityData = [
       "https://s3.ap-south-1.amazonaws.com/thegreatnext-images/images/trips/444/gallery/Gallery_1.jpg"
     ]
   },
-
-<<<<<<< HEAD
-// const cultureData = [
-=======
-  {
-    placeName: "Hawa Mahal ",
-    cityName: "Jaipur,Rajasthan",
-    description:
-      "The Hawa Mahal Jaipur is the beautiful many windowed extension of the City Palace that is said to resemble the honeycomb structure of a bee’s nest. Since the construction of the Hawa Mahal Jaipur in 1799 the building has become the iconic structure of the city and the pink sandstone palace is considered as the finest example of Rajput architecture.",
-    placeCategory: "Historical",
-    images: [
-      "https://www.tourmyindia.com/images/hawa-mahal-fort-jaipur3.jpg",
-      "http://www.petriepr.com/wp-content/uploads/2016/08/Sobha-Niwas.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Jaipur_city_palace_interior.jpg/800px-Jaipur_city_palace_interior.jpg"
-    ]
-  },
-
-  {
-    placeName: "Taj Mahal",
-    cityName: "Agra, Uttar Pradesh",
-    description:
-      "This epitome of Mughal architecture, built entirely with white marble is one of the most well-known identifiers of India. Its grandeur and beauty will have you spellbound.Shah Jahan built this paragon for his wife Mumtaz Mahal to enunciate his love for her. Situated in one of the “Golden Triangle” cities, you also get an opportunity to explore the streets of Agra.",
-    placeCategory: "Historical",
-    images: [
-      "https://www.askideas.com/media/36/Awesome-Taj-Mahal-HD-Wallpapewr.jpg",
-      "https://whc.unesco.org/uploads/thumbs/site_0252_0031-360-360-20131015115549.jpg",
-      "https://api.ning.com/files/sGeqV*sT8d0V4e*2ztJk-jKapZUQ8NN6swNHLQxFGbl-ZqX*S6Vpgv00OgY07PTxh*hvcrDmkG0QysDh6xq501N86vOi292x/AmazingTajmahalPictures2.jpg"
-    ]
-  },
-
-  {
-    placeName: "Gateway Of India",
-    cityName: " Mumbai, Maharashtra",
-    description:
-      " One of India’s iconic landmarks, Gateway of India overlooking the Mumbai Harbour was built by the British in 1924. In the modern times, the Gateway is a favorite picnic spot for locals and tourists, alike.Dotted by dozens of street vendors serving lip-smacking local food along with balloon sellers, give this place a pleasingly happy vibe.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://cdn1.goibibo.com/t_tg_fs/mumbai-gateway-of-india-150051333747-orijgp.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/1/18/Gateway_by_payas.jpg",
-      "http://inde.nouvini.com/wp-content/uploads/sites/5/2016/03/Inde-mumbai-bombay-gateway-india.jpg"
-    ]
-  },
-
-  {
-    placeName: " Mysore Palace",
-    cityName: "Mysore, Karnataka",
-    description:
-      " Built by British architect Lord Henry Irwin in 1912, Mysore Palace is located in the heart of the city. An architectural wonder built in Indo Saracenic style, this palace features intricate interiors. Interestingly,  Golden palanquin was used by the Maharaja which was then carried by elephants during the annual Dasara festivities. Goddess Durga’s idol is now placed inside the palanquin.The months between October to March are the most pleasant and offer a comfortable stay here. Avoid summers as they are very sultry hot.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2016/06/Mysore-Palace-Best-Places-to-Visit-in-Mysore.jpg",
-      "https://www.nativeplanet.com/img/2016/06/illuminatedmysorepalace-24-1466764757.jpg",
-      "http://res.cloudinary.com/himanshujain/image/upload/w_640,h_480,fl_progressive,q_50/v1451838062/activity/Mysure_Palace_-_edited_cekgzm.jpg"
-    ]
-  },
-  {
-    placeName: "Golden Temple",
-    cityName: "Amritsar,Punjab",
-    description:
-      "Tangibly the most spiritual place in India, Golden Temple, also known as Sri Harmindar Sahib, is the holiest shrine in Sikhism which is alive with religious fervour and sacredness. It is a place that can only be experienced and not described. Serving as a symbol of brotherhood and equality, Golden Temple is visited by people from all over the globe who come here to seek spiritual solace and religious fulfilment. Although the Golden Temple itself is of great historical and architectural interest, it is the view of the resplendent shrine, glistening in the centre of the tank, bringing an infinite calmness that is most memorable to a visitor.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://imgak.mmtcdn.com/hp-images/new/cities/1350/Golden%20Temple_704x385.jpg",
-      "https://k6u8v6y8.stackpathcdn.com/blog/wp-content/uploads/2015/04/golden-temple.jpg",
-      "https://i.ytimg.com/vi/m701WKQMeYQ/maxresdefault.jpg"
-    ]
-  },
-  {
-    placeName: "Kumbhalgarh’s Wall",
-    cityName: "Kumbhalgarh,Rajasthan",
-    description:
-      "It took over a century to construct the wall and it was later enlarged in the 19th century. The Kumbhalgarh Fort, protected by the massive wall, was built high on a hill so it could dominate and observe the landscape from a distance. Altogether the wall has seven gateways, and it is believed that during the reign of the Maharana, the wall held so many lamps that it enabled the local farmers to work both day and night.",
-
-    placeCategory: "Historical",
-    images: [
-      "http://www.haja.co/wp-content/uploads/2017/02/World%E2%80%99s-2nd-Largest-Wall-at-Kumbhalgarh-Fort-Rajasthan-4.jpg",
-      "http://www.ancientpages.com/wp-content/uploads/2015/08/greatwallkumbhalgarh5.jpg",
-      "https://www.india.com/travel/wp-content/uploads/2017/12/Kumbhalgarh-photo-6.jpg"
-    ]
-  },
-
-  {
-    placeName: "Charminhar",
-    cityName: "Hyderabad,Telangana",
-    description:
-      "Charminar in Hyderabad was constructed by the eminent architect named Mohammed Quli Qutub Shah in the year of 1591. This magnificent edifice of granite, lime, mortar or pulverised marble was built as soon as he shifted his capital to this city. The monument oozes out sense of aesthetics of the architects of the time. It reflects the great architectural eminence of the time. You can site Charminar - Hyderabad at the heart of the city.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://media-cdn.tripadvisor.com/media/photo-s/10/5e/1e/55/charminar-night-view.jpg",
-      "https://adventurousgopal.files.wordpress.com/2015/03/charminar.jpg",
-      "https://mailz4u.files.wordpress.com/2017/09/charminar.jpg"
-    ]
-  },
-
-  {
-    placeName: "Lake Palace",
-    cityName: "Udaipur, Rajasthan",
-    description:
-      "Taj Lake Palace in Udaipur, is the most romantic hotel in the world.  It was built in 1746 as the pleasure palace of Maharana Jagat Singh II. Taj Lake Palace is one of the most recognisable residences in the world , since it has been featured in many films and TV serials, most notably, Octopussy and The Jewel in The Crown.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/01/shutterstock_536597029.jpg",
-      "https://image3.mouthshut.com/images/imagesp/925003730s.jpg",
-      "https://s-ec.bstatic.com/images/hotel/max1024x768/198/19855153.jpg"
-    ]
-  },
-
-  {
-    placeName: "Chittorgarh Fort",
-    cityName: "Chittorgarh,Rajasthan",
-    description:
-      "Chittorgarh Fort is one of the largest forts in India which was the capital of Rajasthan. Structures like palaces, towers, gates, temples and others can still be found in the fort. The fort has been attacked by many kings of Delhi Sultanate, Gujarat Sultanate, and others.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://www.heritagehotelsofindia.com/blog/wp-content/uploads/2016/11/chittorgarh-fort-chittorgarh.jpg",
-      "http://www.indiaraju.com/wp-content/uploads/2018/06/Chittorgarh-Fort-High-Quality-Wallpapers-min-1500x1108.jpg",
-      "http://allhdwallpapers.com/wp-content/uploads/2015/06/Chittorgarh-Fortindia-5-1024x640.jpg"
-    ]
-  },
-
-  {
-    placeName: "Bada Imambara",
-    cityName: "Lucknow, Uttar Pradesh",
-    description:
-      "The Bara Imambara of Lucknow is one of the most famous monuments of the city. Also known as Asfi Imambara, after the name of the Nawab of Lucknow who got it constructed, it is an important place of worship for the Muslims who come here every year to celebrate the religious festival of Muharram. The Imambara is primarily known for its incredible maze, known as Bhul Bhulaiya locally, which is located on the upper floor of the monument.",
-
-    placeCategory: "Historical",
-    images: [
-      "https://ik.imagekit.io/rkw3fpbem/wp-content/uploads/2018/02/bara-imambara-complex-lucknow.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/a/ae/Bara_Imambara_At_Night_01.JPG",
-      "https://res.cloudinary.com/trell/f_auto,fl_progressive,t_medium/user-content/images/orig/ZYeJazurpmh2nQBQpHMs.jpg"
-    ]
-  },
-
-  {
-    placeName: "Shalimar Bagh",
-    cityName: "Shrinagar, Jammu and Kashmir",
-    description:
-      "Covering 12.4 hectares of land, Shalimar Garden adds to the beauty of Srinagar. A chinar-lined canal passes through the middle of the well-organized garden offering an immaculate picture of Mughal creation. Divided into threemain terraces, Shalimar Bagh blends natural beauty with architectural genius and offers itself as a prime attraction in Jammu & Kashmir tourism. Each terrace has been designed carefully and displays a colorful picture; the first terrace comprises Diwan-e-Aam, whereas the second terrace shelters Diwan-e-Khas. Attracting people of all ages, this 400 years old garden is a wonderful tourist place to visit in Srinagar. Overcoming many tides of time, this exquisite Mughal marvel has been able to maintain its timeless beauty.",
-
-    placeCategory: "Historical",
-    images: [
-      "[http://www.thekashmirtravels.com/wp-content/uploads/2018/01/1389870295.jpg",
-      "https://www.tourmyindia.com/images/shalimar_bag.jpg",
-      "http://www.transindiatravels.com/wp-content/uploads/shalimar-bagh-jammu-and-kashmir.jpg"
-    ]
-  }
-];
-
+]
 // const cultureData = [
 //   {
 //     festivalName: "Diwali",
@@ -408,75 +250,6 @@ const cityData = [
 //   }
 // ];
 
-// const foodData = [
->>>>>>> 82814d91af7f833e9b2046ea0c75ca889ebc3d01
-//   {
-//     festivalName: "Diwali",
-//     Mounth: "November",
-//     description:
-//       "Millions of Hindus around the world celebrate Diwali with gift exchanges, fireworks and festive meals. The Diwali celebration in India takes place when the monsoon season ends and the weather is mild and pleasant. People try to pay off their old debts, make or buy new clothes and thoroughly clean their houses as part of the festival preparations. House exteriors are whitewashed and sometimes decorated with designs drawn in white rice flour and filled in with color. Buildings are traditionally illuminated with oil-burning bowls called dipa lights, or more recently, with strings of artificial lights. People spend time with their friends and family.",
-//     category: "Culture",
-//     images: [
-//       "https://dama.bg/uploaded/posts/dbd914804353a3572c4c5a61dc655d65.jpg?v=1",
-//       "https://images.indianexpress.com/2017/06/29-vs-dipawali-celebration-759.jpg",
-//       "http://temesira.org/wp-content/uploads/2017/07/Happy-Diwali-2016.png"
-//     ]
-//   },
-
-//   {
-//     festivalName: "Holi",
-//     Mounth: "March",
-//     description:
-//       "Holi also celebrates the victory of good over evil, as well as the abundance of the spring harvest season. It's commonly referred to as the 'Festival of Colors'. People exuberantly throw colored powder and water all over each other, have parties, and dance under water sprinklers. Bhang (a paste made from cannabis plants) is also traditionally consumed during the celebrations. Holi is a very carefree festival that's great fun to participate in if you don’t mind getting wet and dirty.",
-//     category: "Culture",
-//     images: [
-//       "https://visittrivalley.com/wp-content/uploads/2018/03/iStock-471366604.jpg",
-//       "https://www.visitrenotahoe.com/wp-content/uploads/2017/07/HoliFestivalColor1.jpg",
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Holi_shop.jpg/1200px-Holi_shop.jpg"
-//     ]
-//   },
-
-//   {
-//     festivalName: "Dussehra",
-//     Mounth: "September",
-//     description:
-//       "Dussehra, also referred to as Vijayadashami, is also among the most famous festivals of India. It is celebrated in different forms countrywide. Ramlila (enactment of scenes from Ramayana) is held everywhere for 10 days. It’s culminated with “Ravan Dahan” – the burning of huge effigies of Ravana, Meghnath, and Kumbhkaran which is a real spectacle to see.",
-//     category: "Culture",
-//     images: [
-//       "https://d19gb5k9ejx8w0.cloudfront.net/uploads/2014/10/CoverPage-Dussehra-Ramlila.jpg",
-//       "http://images.patrika.com/mediafiles/2016/09/12/l_mela-1473655295.jpg",
-//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSj6qve2pQUoyjAtqTbQcfal0CI1NB8VHOe2W3xNs04JEFr5yaXghttp://blog.railyatri.in/wp-content/uploads/2015/10/blog_Mysore-dasara.png"
-//     ]
-//   },
-
-//   {
-//     festivalName: "Vaisakhi",
-//     Mounth: "April",
-//     description:
-//       "Baisakhi is primarily a festival celebrated by the Sikh community of Punjab and those around the world. It celebrates the welcoming of the harvest season for the rabi crops. The Sikhs celebrate this festival with a lot of excitement and enthusiasm by performing local folk dances such as Giddha and Bhangra. The festival is of great religious significance in India as it marks the day when the tenth Guru of Sikhs, Guru Gobind Singh, laid out the foundation stone for the Panth Khalsa-the Order back in 1699.",
-//     category: "Culture",
-//     images: [
-//       "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2017/12/cover2.jpg",
-//       "http://www.thetravelboss.com//userfiles/image/images/festivities/Baisakhi-Festival-3.jpg",
-//       "http://www.4to40.com/wp-content/uploads/2016/01/Young-girls-wear-traditional-Punjabi-dress-as-they-perform-the-giddha-during-celebrations-on-the-eve-of-the-Lohri-festival-in-Amritsar.jpg"
-//     ]
-//   },
-
-//   {
-//     festivalName: "Ganesh Chaturthi",
-//     Mounth: "September",
-//     description:
-//       "The spectacular Ganesh Chaturthi festival honors the birth of the beloved Hindu elephant-headed god, Lord Ganesha. The start of the festival sees huge, elaborately crafted statutes of Ganesha installed in homes and public podiums, which have been especially constructed and beautifully decorated. At the end of the festival, the statutes are paraded through the streets, accompanied by much singing and dancing, and then submerged in the ocean. The best place to experience it is in Mumbai.",
-//     category: "Culture",
-//     images: [
-//       "https://i.dailymail.co.uk/i/pix/2010/09/22/article-1314347-0B4E98AA000005DC-515_972x488.jpg",
-//       "http://www.blogiism.com/wp-content/uploads/2017/08/ganesh-Chaturthi-gujarat.jpg",
-//       "https://s3.amazonaws.com/medias.photodeck.com/493e5156-f46b-43fc-a993-1f90b7772817/Brett-Cole-India-02391_medium.jpg"
-//     ]
-//   }
-// ];
-
-<<<<<<< HEAD
 const foodData = [
   {
     foodName: "Paneer Tikka Masala",
@@ -563,11 +336,6 @@ const foodData = [
 Food.create(foodData)
   .then(foodResults => {
     console.log(`Inserted ${foodResults.length} food`);
-=======
-City.create(cityData)
-  .then(cityResults => {
-    console.log(`Inserted ${cityResults.length} City`);
->>>>>>> 82814d91af7f833e9b2046ea0c75ca889ebc3d01
   })
   .catch(err => {
     console.log("inserted failure", err);
