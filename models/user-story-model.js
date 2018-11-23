370275889a02a48fb426566b3e976af6a8944f3e
Harnitks <harnitks@gmail.com>
2018-11-23 14:30:12 +0100

    fixed story model

--- models/user-story-model.js
+++ models/user-story-model.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const storySchema = new Schema({
-  user: {type:String, ref:"User",required:true},
-  writerName:{type:String},
-  title:{type:String},
-  description: {type:String},
-  place:{type:String},
-  avatar: {type: String},
- });
+  user: { type: String, required: true },
+  writerName: { type: String },
+  title: { type: String },
+  description: { type: String },
+  place: { type: String },
+  avatar: { type: String }
+});
 
-const Story = mongoose.model("Story",storySchema);
+const Story = mongoose.model("Story", storySchema);
 
 module.exports = Story;
--- routes/story-router.js
+++ routes/story-router.js
@@ -1,77 +1,85 @@
 const express = require("express");
 const router = express.Router();
 const Story = require("../models/user-story-model.js");
-const fileUploader = require("../config/file-upload.js")
+const fileUploader = require("../config/file-upload.js");
 
 //this is for listing of stories
 router.get("/see-story/stories", (req, res, next) => {
-  if(req.user == undefined){
+  if (req.user == undefined) {
     res.redirect("/login");
     return;
   }
-  
-  Story.find().then(storiesResult => {
-    //res.send(mysteriousresults)
-    res.locals.arrayOfStories = storiesResult;
-    res.render("see-story/show-story.hbs");
-  })
-  .catch(err => next(err));
+
+  Story.find()
+    .then(storiesResult => {
+      //res.send(mysteriousresults)
+      res.locals.arrayOfStories = storiesResult;
+      res.render("see-story/show-story.hbs");
+    })
+    .catch(err => next(err));
 });
 
-router.get("/allStories",(req,res,next)=>{
-  if(req.user == undefined){
+router.get("/allStories", (req, res, next) => {
+  if (req.user == undefined) {
     res.redirect("/login");
     return;
   }
-  
-  Story.find().then(storiesResult => {
-    //res.send(mysteriousresults)
-    res.locals.arrayOfStories = storiesResult;
-    res.render("see-story/show-story.hbs");
-  })
-  .catch(err => next(err));
-});
 
+  Story.find()
+    .then(storiesResult => {
+      //res.send(mysteriousresults)
+      res.locals.arrayOfStories = storiesResult;
+      res.render("see-story/show-story.hbs");
+    })
+    .catch(err => next(err));
+});
 
 //this is for story detail
 router.get("/see-story/:storyId", (req, res, next) => {
-  if(req.user == undefined){
+  if (req.user == undefined) {
     res.redirect("/login");
     return;
   }
-  const {storyId} = req.params;
-  Story.findById(storyId).then(storyDoc => {
-    res.locals.story = storyDoc;
-    res.render("see-story/story-detail.hbs");
-  })
-  .catch(err => next(err));
+  const { storyId } = req.params;
+  Story.findById(storyId)
+    .then(storyDoc => {
+      res.locals.story = storyDoc;
+      res.render("see-story/story-detail.hbs");
+    })
+    .catch(err => next(err));
 });
 
-router.post("/process-story", 
-fileUploader.single("avatarUpload"), 
-(req, res, next) => {
-  console.log("-------------------------------hello");
-  console.log("req file ", req.file);
+router.post(
+  "/process-story",
+  fileUploader.single("avatarUpload"),
+  (req, res, next) => {
+    console.log("-------------------------------hello");
+    console.log("req file ", req.file);
 
+    const { user, writerName, title, description, place } = req.body;
 
-  const { user,writerName,title,description,place } = req.body;
+    let toUpdate = { user, writerName, title, description, place };
+    //multer stores the file in req.file
+    console.log("req file ", req.file);
 
-  let toUpdate = { user,writerName,title, description,place};
-  //multer stores the file in req.file
-  console.log("req file ", req.file);
- 
-  if (req.file) {
-    console.log("req file secure url ", req.file.secure_url);
-    toUpdate = {  user,writerName,title, description,place, avatar: req.file.secure_url };
+    if (req.file) {
+      console.log("req file secure url ", req.file.secure_url);
+      toUpdate = {
+        user,
+        writerName,
+        title,
+        description,
+        place,
+        avatar: req.file.secure_url
+      };
+    }
+
+    Story.create(toUpdate)
+      .then(storyDoc => {
+        res.redirect("/see-story/stories");
+      })
+      .catch(err => next(err));
   }
-
-
-
-  Story.create(toUpdate).then(storyDoc=>{
-    res.redirect("/see-story/stories")
-  })
-  .catch(err =>next(err));
-});
-
+);
 
 module.exports = router;
